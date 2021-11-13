02cae417cbeca270fe76ca93c5600ed084e142b0
DeyAkansha <deyaku2000@gmail.com>
2021-11-12 20:41:03 -0500

    css

--- soundboard.js
+++ soundboard.js
@@ -1,7 +1,17 @@
-
 var buttonElement = document.getElementById('button-1')
 buttonElement.addEventListener('click', function () {
-    console.log('I was clicked!')
-    var snd = new Audio("applause_y.wav")
-    snd.play()
+    var snd = new Audio("applause_y.wav");
+    snd.play();
+})
+
+var buttonElement1 = document.getElementById('button-2')
+buttonElement1.addEventListener('click', function () {
+    var snd1 = new Audio("bubbles_sfx.wav");
+    snd1.play();
+})
+
+var buttonElement2 = document.getElementById('button-3')
+buttonElement2.addEventListener('click', function () {
+    var snd2 = new Audio("buzzer_x.wav");
+    snd2.play();
 })
